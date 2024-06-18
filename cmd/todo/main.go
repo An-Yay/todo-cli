4872b4f96a49a69b57f7416cf5085269bf66f15b
Ananay Gupta <ananaygupta23@gmail.com>
2024-06-19 02:31:47 +0530

    added command flags

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,11 +1,29 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+	todo "todo/cmd"
+)
 
 const (
 	todofile = ".todo.json"
 )
 
 func main() {
-	add := flag.Bool
+	add := flag.Bool("add", false, "add a new task")
+	complete := flag.Int("complete", 0, "mark a task as completed")
+	del := flag.Int("del", 0, "delete a task")
+	list := flag.Bool("list", false, "list all tasks")
+
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todofile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
 }
